6a7d9f79851e21d0822b9d9adb7c1b2a1ef82017
Vimal Anand <78069624+Vimal2023@users.noreply.github.com>
2024-03-01 09:55:36 +0000

    done with variables

---
+++ 01_basics/var-const-let.js
@@ -0,0 +1,16 @@
+const accountId = 143526
+let accountEmail = "qwer123@gmail.com"
+var accountPassword = "12345"
+accountcity = "Darbhanga" // not to use
+let accountState;
+
+// accountId = 2 // not allowed to change 
+
+accountEmail = "123efrdg@gmail.com"
+
+console.log(accountEmail);
+
+accountPassword = "21212121"
+accountcity = "patna"
+
+console.table([accountId, accountEmail, accountPassword, accountcity, accountState]);
