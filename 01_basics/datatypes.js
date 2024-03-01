2709a999d2faa6e26dc3724cfc055cd8850a6715
Vimal Anand <78069624+Vimal2023@users.noreply.github.com>
2024-03-01 10:31:45 +0000

    datatypes done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS Code as newer version
+
+let name = "Vimal"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to the power 53
+// big int
+// string => ""
+//NULL => stadalone Value , undefine nahi ye bas khaali hai 
+// undefined => value not assigned
+
+//symbol => unique 
+
+// object 
+
+console.log(typeof null);
+console.log(typeof undefined);
