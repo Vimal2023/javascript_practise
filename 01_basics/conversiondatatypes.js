dba8bcea089e2c096107f3f320eee1f6d119e8e9
Vimal Anand <78069624+Vimal2023@users.noreply.github.com>
2024-03-02 18:28:33 +0000

    done

---
+++ 01_basics/conversiondatatypes.js
@@ -0,0 +1,25 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+
+let isLoggedIn = 1
+
+let booleanLoggedIn = Boolean (isLoggedIn)
+ // console.log(booleanLoggedIn);
+
+ // 1 => true ; 0 => false
+ // "" => false
+ // "vimal" => true
+
+ let someNumber = 33
+ let stringNumber = String (someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
