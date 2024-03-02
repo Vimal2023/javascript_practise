3372fd921755c1301f11c73c51b389cc7f79f95e
Vimal Anand <78069624+Vimal2023@users.noreply.github.com>
2024-03-02 19:06:18 +0000

    confusion cleared

--- 01_basics/conversiondatatypes.js
+++ 01_basics/conversiondatatypes.js
@@ -21,5 +21,25 @@ let booleanLoggedIn = Boolean (isLoggedIn)
 
  let someNumber = 33
  let stringNumber = String (someNumber)
- console.log(stringNumber);
- console.log(typeof stringNumber);
+ // console.log(stringNumber);
+ // console.log(typeof stringNumber);
+
+ let value = 3
+ let negValue = -value
+ // console.log(negValue);
+
+ let str1 = "Vimal"
+ let str2 = " Anand"
+ let str3 = str1 + str2
+
+ // console.log(str3)
+
+ //console.log("1" + 2); // 12
+ //console.log("1" + 2 + 2); // 122
+ //console.log(1 + "2"); // 12
+ //console.log(1 + 2 + "2"); // 32
+
+ //console.log(+true); //1
+ //console.log(+""); // 0
+
+ 
