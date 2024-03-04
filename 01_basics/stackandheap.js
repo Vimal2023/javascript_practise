52cce445f290417390af12fd624b2f2f7a9776ab
Vimal Anand <78069624+Vimal2023@users.noreply.github.com>
2024-03-04 13:48:21 +0000

    memory location

---
+++ 01_basics/stackandheap.js
@@ -0,0 +1,18 @@
+// stack(Primitive)  and heap(non-primitive)
+
+let myName = "Vimal Anand"
+let anotherName = myName 
+
+anotherName = "Anshu"
+
+// console.log(myName);
+// console.log(anotherName);
+
+let userOne = {
+    email : "user@gmail.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne 
+//console.log(userOne.email);
+// console.log(userTwo.email);
